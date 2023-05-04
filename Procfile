374864ec1c855cae8dcd6255c7076b4376324ab4
juttayaya <admin@apps.jirawat.com>
2023-05-04 08:07:37 -0400

    Custom Caddy buildpack

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: chmod 755 ./setup_caddyfile.sh && ./setup_caddy.sh && ./caddy -conf="Caddyfile"
---
+++ setup_caddy.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+echo "In setup_caddy.sh"
+#echo 0.0.0.0:$PORT > HerokuCaddyfile
+#echo "import ./Caddyfile.heroku" >> HerokuCaddyfile
