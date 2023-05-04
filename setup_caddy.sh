aad184e5e9ea99037e47ce8db6015b435ea94aca
juttayaya <admin@apps.jirawat.com>
2023-05-04 09:26:13 -0400

    ERB caddy

--- setup_caddy.sh
+++ setup_caddy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 echo "In setup_caddy.sh converting Caddyfile.erb"
-erb ./config/Caddyfile.erb > ./Caddyfile
+erb ./Caddyfile.erb > ./Caddyfile
 #echo 0.0.0.0:$PORT > HerokuCaddyfile
 #echo "import ./Caddyfile.heroku" >> HerokuCaddyfile
